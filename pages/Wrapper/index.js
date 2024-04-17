23d244d66b1be21cfb80e233248c0f22cc88a9f8
Jeremy Smith <prez@Jeremys-MacBook-Air.local>
2024-04-17 11:59:04 -0700

    wip

---
+++ pages/Wrapper/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'reactstrap';
+import BaseLayout from '../components/layouts/BaseLayouts';
+
+const MainStyle = {
+  overflowX: 'hidden',
+};
+
+const copyStyle = {
+  fontSize: '22px',
+  fontFamily: 'avenir',
+  textAlign: 'center',
+};
+
+export default function Wrapper() {
+  return (
+    <BaseLayout>
+      <div id="idxStart"></div>
+    <div id="idxStop"></div>
+    </BaseLayout>
+  );
+}
